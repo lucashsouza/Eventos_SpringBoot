960a4ffc531b96d75ede009345efe874f947c5b1
Lucas Souza <lhenriquesouza00@gmail.com>
2021-01-23 11:50:26 -0300

    Curso Spring Boot aula 03: Criando e configurando um banco de dados MySQL
    Alterado GenerationType de "auto" para "Identity", evitando a criação da tabela  de suporte "hibernate_sequence"

--- eventoapp/src/main/java/br/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/br/com/eventoapp/models/Evento.java
@@ -12,7 +12,7 @@ public class Evento implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
 
     private String nome;
