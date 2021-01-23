783031134523331902dd8beb8601b0db814c305c
Lucas Souza <lhenriquesouza00@gmail.com>
2021-01-23 12:07:26 -0300

    Curso Spring Boot aula 04: Salvando dados do formulário no banco de dados

---
+++ eventoapp/src/main/java/br/com/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,8 @@
+package br.com.eventoapp.repository;
+
+import br.com.eventoapp.models.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento, String> {
+
+}
