7ef1d65caf0007c7f72370c906c6b7aa18e79b70
Lucas Souza <lhenriquesouza00@gmail.com>
2021-01-24 04:56:36 -0300

    Curso Spring Boot aula 08: Criando requisições especificas com a anotação @PathValiable

--- eventoapp/src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import br.com.eventoapp.models.Evento;
 import br.com.eventoapp.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,6 +33,16 @@ public class EventoController {
         ModelAndView mv = new ModelAndView("index");
         Iterable<Evento> eventos = er.findAll();
         mv.addObject("eventos", eventos);
+        return mv;
+    }
+
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+        Evento evento = er.findByCodigo(codigo);
+
+        ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+        mv.addObject("evento", evento);
+        System.out.println("evento" + evento);
         return mv;
     }
 }
--- eventoapp/src/main/java/br/com/eventoapp/repository/EventoRepository.java
+++ eventoapp/src/main/java/br/com/eventoapp/repository/EventoRepository.java
@@ -4,5 +4,6 @@ import br.com.eventoapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
+    Evento findByCodigo(long codigo);
 
 }
